ab4dea6b4017b01c6af05193efa4699e3ce12176
icarodr <it46009@gmail.com>
2022-10-23 19:07:45 -0300

    add new component

---
+++ frontbank/components/BottomBar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BottomBar = () => {
+    return(
+        <div className="text-black text-4xl">
+            <h1>Bottom Bar</h1>
+        </div>
+    )
+}
+
+export default BottomBar
